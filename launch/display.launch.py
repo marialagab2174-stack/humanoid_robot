70c4d21fc6692d8951014317f6f7b068f4237b0e
Maria Lagab <marialagab2174@gmail..com>
2026-05-03 19:32:09 +0100

    Envoi du projet Robot Humanoïde : Conception et Simulation

--- launch/display.launch.py
+++ launch/display.launch.py
@@ -3,16 +3,22 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.substitutions import Command
+from launch_ros.parameter_descriptions import ParameterValue
 
 def generate_launch_description():
     pkg_path = get_package_share_directory('humanoid_robot')
     urdf_file = os.path.join(pkg_path, 'urdf', 'humanoid.urdf')
 
+    robot_description = ParameterValue(
+        Command(['cat ', urdf_file]),
+        value_type=str
+    )
+
     return LaunchDescription([
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
-            parameters=[{'robot_description': Command(['cat ', urdf_file])}]
+            parameters=[{'robot_description': robot_description}]
         ),
         Node(
             package='joint_state_publisher_gui',
