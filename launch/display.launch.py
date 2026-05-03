c7902fea194e1a9f61d74714023bde492a048bbe
Maria Lagab <marialagab2174@gmail..com>
2026-05-03 18:50:40 +0100

    Envoi du projet Robot Humanoïde : Conception et Simulation

--- launch/display.launch.py
+++ launch/display.launch.py
@@ -2,22 +2,17 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.substitutions import Command
 
 def generate_launch_description():
-    # 1. Trouver le chemin exact du package
     pkg_path = get_package_share_directory('humanoid_robot')
     urdf_file = os.path.join(pkg_path, 'urdf', 'humanoid.urdf')
 
-    # 2. Lire le fichier URDF proprement
-    with open(urdf_file, 'r') as infp:
-        robot_desc = infp.read()
-
-    # 3. Lancer les 3 outils
     return LaunchDescription([
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
-            parameters=[{'robot_description': robot_desc}]
+            parameters=[{'robot_description': Command(['cat ', urdf_file])}]
         ),
         Node(
             package='joint_state_publisher_gui',
